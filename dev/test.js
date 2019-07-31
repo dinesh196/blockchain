88a6eca10f755d15c6940a8eb519d47991cb20dd
dinesh196 <chavandinesh196@gmail.com>
2019-07-31 18:53:05 +0530

    Basic blockchain with api server

---
+++ dev/test.js
@@ -0,0 +1,49 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+console.log(bitcoin);
+
+
+// const previousBlockHash = "GhjGDJHGSJHg867VHJVJ76VHJ";
+// const currebtBlockData = [
+//     {
+//         amount: 101,
+//         sender: 'FGHFG7687FCGH67868FVFHG',
+//         recipient: 'SDK7DSUTD7S8TG7US8ID'
+//     },
+//     {
+//         amount: 30,
+//         sender: 'FG323HFG7687FCGH67868FVFHG',
+//         recipient: 'SDFDADFK7DSUTD7S8TG7US8ID'
+//     },
+//     {
+
+//         amount: 200,
+//         sender: 'FCGH67868FVFHG',
+//         recipient: 'TD7S8TG7US8ID'
+//     }
+// ];
+// // const nonce = 100;
+// console.log(bitcoin.hashBlock(previousBlockHash,currebtBlockData,71115));
+//console.log( bitcoin.proofOfWork(previousBlockHash,currebtBlockData));
+
+// bitcoin.createNewBlock(123,"shahjasd","jsajl");
+// // bitcoin.createNewBlock(12233,"sha23hjasd","jsajl");
+// bitcoin.createNewTransaction(120,"appleghsgjh","bananasahkfjjag");
+
+// bitcoin.createNewBlock(12213,"sha12321hjasd","safssjsajl");
+
+// bitcoin.createNewTransaction(1220,"appleghsgjh","bananasahkfjjag");
+
+// bitcoin.createNewTransaction(12120,"appleghsgjh","bananasahkfjjag");
+
+// bitcoin.createNewTransaction(12012,"appleghsgjh","bananasahkfjjag");
+
+// bitcoin.createNewTransaction(122310,"appleghsgjh","bananasahkfjjag");
+
+// bitcoin.createNewBlock(6969,"sha12321hjasd","safssjsajl");
+
+// //console.log(bitcoin);
+// console.log(bitcoin.chain[2])
+
